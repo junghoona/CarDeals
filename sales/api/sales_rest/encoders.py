754b3c2e40f0cceea5e17e02a7c4d9016db618c7
John An <junghooonan@gmail.com>
2023-07-24 18:46:37 -0700

    [Final Proj]: added encoders

---
+++ sales/api/sales_rest/encoders.py
@@ -0,0 +1,51 @@
+from common.json import ModelEncoder
+
+from .models import (
+    AutomobileVO,
+    Salesperson,
+    Customer,
+    Sale
+)
+
+
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "vin",
+        "sold",
+        "id",
+    ]
+
+
+class SalespersonEncoder(ModelEncoder):
+    model = Salesperson
+    properties = [
+        "first_name",
+        "last_name",
+        "employee_id",
+    ]
+
+
+class CustomerEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        "last_name",
+        "address",
+        "phone_number",
+    ]
+
+
+class SaleEncoder(ModelEncoder):
+    model = Sale
+    properties = [
+        "price",
+        "automobile",
+        "salesperson",
+        "customer",
+    ]
+    encoders = {
+        "automobile": AutomobileVOEncoder(),
+        "salesperson": SalespersonEncoder(),
+        "customer": CustomerEncoder(),
+    }
