13ddcb7270dddbb636dd9edb3213544c48689b94
Julius Newell <newelljulius@gmail.com>
2023-07-26 13:52:07 -0700

    Service History within Appointments page. Search bar filter works

--- ghi/app/src/ServiceAppointmentList.js
+++ ghi/app/src/ServiceAppointmentList.js
@@ -3,33 +3,43 @@ import React, { useEffect, useState  } from 'react';
 function ServiceAppointmentList () {
     const [appointments, setAppointments] = useState([]);
     const [technicians, setTechnicians] = useState([]);
+    const [vinSearch, setVinSearch] = useState("");
+    const [vinResult, setVinResult] = useState([]);
 
     const fetchData = async () => {
         const response = await fetch("http://localhost:8080/api/appointments/");
-
         if(response.ok) {
             const data = await response.json();
             setAppointments(data.appointments);
         }
-
-    }
+    };
 
     const fetchTechnicians = async () => {
         const response = await fetch("http://localhost:8080/api/technicians/");
-    
         if (response.ok) {
           const data = await response.json();
           setTechnicians(data.technicians);
         }
-      }
+      };
 
     useEffect(() => {
         fetchData();
         fetchTechnicians();
     }, []);
 
-    const cancel = async (appointment) => {
+// Handles search input field
+    const handleVinSearch = (event) => {
+        setVinSearch(event.target.value);
+    };
 
+// Handles Search Button click event
+    const handleSearchButton = () => {
+        const search = appointments.filter(appointment => appointment.vin === vinSearch);
+        setVinResult(search);
+      };
+
+// "PUT" request to the cancel url
+    const cancel = async (appointment) => {
         const url = `http://localhost:8080/api/appointments/${appointment.id}/cancel`
         const fetchConfig = {
             method: 'put',
@@ -38,9 +48,8 @@ function ServiceAppointmentList () {
             },
             body: JSON.stringify({ satus: 'canceled' }),
         };
-
         const response = await fetch(url, fetchConfig);
-        const data = await response.json();
+// Updates Status column field to 'canceled' when cancel button is clicked
         if (response.ok) {
             setAppointments((prevAppointments) =>
               prevAppointments.map((appt) =>
@@ -49,7 +58,8 @@ function ServiceAppointmentList () {
             );
           }
         }
-
+    
+// "PUT" request to finish url
     const finish = async (appointment) => {
         const url = `http://localhost:8080/api/appointments/${appointment.id}/finish`
         const fetchConfig = {
@@ -59,7 +69,7 @@ function ServiceAppointmentList () {
             },
         };
         const response = await fetch(url, fetchConfig);
-        const data = await response.json();
+// Updates Status column field to 'finished' when finish button is clicked
         if (response.ok) {
             setAppointments((prevAppointments) =>
               prevAppointments.map((appt) =>
@@ -69,47 +79,81 @@ function ServiceAppointmentList () {
           }
         }
 
+// Find technician by id & returns full name
     const TechnicianName = (technicianId) => {
         const technician = technicians.find(tech => tech.employee_id === technicianId);
         return technician ? `${technician.first_name} ${technician.last_name}` : "";
       };
 
     return (
-        <div className="container m-3">
-        <div>
-            <h1>Service Appointments</h1>
+      <div className="container m-3">
+        <h1>Service Appointments</h1>
+        <div className="d-flex flex-column">
+          <div className="row mb-3">
+            <div className="col">
+              <div className="input-group">
+                <input type="text" placeholder="Search by VIN..." value={vinSearch} onChange={handleVinSearch} className="form-control" />
+                <div className="input-group-append">
+                  <button onClick={handleSearchButton} type="button" className="btn btn-outline-secondary">Search</button>
+                </div>
+              </div>
+            </div>
+          </div>
             <table className='table table-striped'>
             <thead>
-                <tr>
-                    <th>VIN</th>
-                    <th>Customer</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Technician</th>
-                    <th>Reason</th>
-                    <th>Status</th>
-                </tr>
+              <tr>
+                <th>VIN</th>
+                <th>Customer</th>
+                <th>Date</th>
+                <th>Time</th>
+                <th>Technician</th>
+                <th>Reason</th>
+                <th>Status</th>
+              </tr>
             </thead>
             <tbody>
-                {appointments.map(appointment => {
-                    return (
-                        <tr key={appointment.vin}>
-                            <td>{appointment.vin}</td>
-                            <td>{appointment.customer}</td>
-                            <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
-                            <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
-                            <td>{TechnicianName(appointment.technician)}</td>
-                            <td>{appointment.reason}</td>
-                            <td>{appointment.status}</td>
-                            <td><button type="button" className="btn btn-danger" onClick={()=>cancel(appointment)}>Cancel</button></td>
-                            <td><button type="button" className="btn btn-success" onClick={()=>finish(appointment)}>Finish</button></td>
-                        </tr>
-                    )
-                })}
+{/* Filtered appointments */}
+              {vinResult.length > 0 ? (vinResult.map(appointment => (
+                <tr key={appointment.vin}>
+                    <td>{appointment.vin}</td>
+                    <td>{appointment.customer}</td>
+                    <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
+                    <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
+                    <td>{TechnicianName(appointment.technician)}</td>
+                    <td>{appointment.reason}</td>
+                    <td>{appointment.status}</td>
+                    <td>
+                    <button type="button" className="btn btn-danger" onClick={() => cancel(appointment)}>Cancel</button>
+                    </td>
+                    <td>
+                    <button type="button" className="btn btn-success" onClick={() => finish(appointment)}>Finish</button>
+                    </td>
+                </tr>
+                ))
+                ) : (
+// If search input is empty, list all appointments
+                appointments.map(appointment => (
+                <tr key={appointment.vin}>
+                    <td>{appointment.vin}</td>
+                    <td>{appointment.customer}</td>
+                    <td>{new Date(appointment.date_time).toLocaleDateString()}</td>
+                    <td>{new Date(appointment.date_time).toLocaleTimeString()}</td>
+                    <td>{TechnicianName(appointment.technician)}</td>
+                    <td>{appointment.reason}</td>
+                    <td>{appointment.status}</td>
+                    <td>
+                    <button type="button" className="btn btn-danger" onClick={() => cancel(appointment)}>Cancel</button>
+                    </td>
+                    <td>
+                    <button type="button" className="btn btn-success" onClick={() => finish(appointment)}>Finish</button>
+                    </td>
+                </tr>
+                )))
+              }
             </tbody>
-        </table>
+            </table>
         </div>
-        </div>
+      </div>
     )
 }
 
